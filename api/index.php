1b596f148079d3eae03867f48539118cbd886d12
Everton Inocencio <hewerthomn@gmail.com>
2013-04-15 00:00:42 -0400

    api de temperatura

--- api/index.php
+++ api/index.php
@@ -2,4 +2,16 @@
 /**
  * API Index
  */
-echo "Hello World!";
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+$service = $_GET['s'];
+
+require 'temperature.php';
+
+$data = get_temperature();
+
+$result = "<{$data['city']}|{$data['temp']}|{$data['time']}>";
+
+die($result);
