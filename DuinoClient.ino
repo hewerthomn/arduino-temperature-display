37ee39f6b5691ad138422537268c89580812656e
Everton Inocencio <hewerthomn@gmail.com>
2013-04-15 20:01:47 -0400

    adicionado beerware license

--- DuinoClient.ino
+++ DuinoClient.ino
@@ -1,3 +1,12 @@
+
+/*
+* ----------------------------------------------------------------------------
+* "THE BEER-WARE LICENSE" (Revision 42):
+* <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+* can do whatever you want with this stuff. If we meet some day, and you think
+* this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+* ----------------------------------------------------------------------------
+*/
 /*
 * DuinoClient
 *  
@@ -8,6 +17,7 @@
 *  Ethernet shield attached to pins 10, 11, 12, 13
 *  LCD Matrix 16x2 attached to pins 
 */
+
 
 #include <SPI.h>
 #include <Ethernet.h>
