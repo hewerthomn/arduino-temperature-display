8c4060d214cf519bcd34885cf9243d12468fd245
Everton Inocencio <hewerthomn@gmail.com>
2013-04-16 23:22:48 -0400

    incluido metodo de buscar temperatura na index

--- api/index.php
+++ api/index.php
@@ -9,7 +9,44 @@ header('Content-type: application/json');
 $service = $_GET['s'];
 date_default_timezone_set('America/Porto_Velho');
 
-require 'temperature.php';
+/**
+ * Usa a API da wunderground para chckar a 
+ * @return array
+ */
+function get_temperature() {
+    /**
+     * create a account and get your API key at
+     * http://www.wunderground.com/
+     */
+    $APIKEY = "";
+    //$json_string = file_get_contents("http://api.wunderground.com/api/{$APIKEY}/conditions/q/BR/Porto_Velho.json");
+    $json_string = file_get_contents("http://api.wunderground.com/api/{$APIKEY}/conditions/q/SBPV.json");
+    
+    $parsed_json = json_decode($json_string);
+
+    $location  = $parsed_json->{'current_observation'}->{'display_location'}->{'city'};
+    $location .= '/' . $parsed_json->{'current_observation'}->{'display_location'}->{'country_iso3166'};
+    $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
+    $relative_humidity = $parsed_json->{'current_observation'}->{'relative_humidity'};
+
+    /**
+     * preenche com espaços se necessário até o tamanho indicado
+     */
+    $location = str_pad($location, 16);
+    $temperature  = str_pad("{$relative_humidity}H  {$temp_c}C", 11);
+
+    return array(        
+        'city' => $location,
+        'temp' => $temperature,
+        'time' => date('h:i')
+    );
+}
+
+// $data = array(
+//  'city' => 'ABCDEDFGHIJKLMNO',
+//  'temp' => '89%H  29C  ',
+//  'time' => '12:10'
+// );
 
 $data = get_temperature();
 
